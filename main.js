7dfb089f2d795a483f5e6e0f6ab8a60f0809746e
Sunita <54287247+sunitase@users.noreply.github.com>
2019-09-24 18:04:21 -0700

    Submit all tests done long time back

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
 // write your code here
+    $.getJSON("data.json", function(data){
+        var hotspot_data = '';
 
+        $.each(data, function(key,value){
+            var link_it = "<a href= 'https://www.google.com/maps?q=" + value.location + "'>" + 'Link' + '</a>';
+            hotspot_data += '<tr>';
+            hotspot_data += '<td>' + value.name + '</td>';
+            hotspot_data += '<td>' + value.description + '</td>';
+            //hotspot_data += '<td>' + value.location + '</td>';
+            hotspot_data += '<td>' + link_it + '</td>';
+            hotspot_data += '</tr>';
+        });
+    $('tbody').append(hotspot_data);
+    
+    });
 });
+
+
+
+
